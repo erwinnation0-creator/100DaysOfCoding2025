8cfe9698f21bfb8fe343fefcae73f273889ee54e
erwinnation0-creator <erwinnation0@gmail.com>
2025-09-10 13:47:42 +0800

    Create Days03.java

---
+++ Days03.java
@@ -0,0 +1,14 @@
+package pkg3daysofcoding;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //komentar satu baris
+        
+        /*komentar 
+        lebih dari
+        satu baris
+        */
+    }
+    
+}

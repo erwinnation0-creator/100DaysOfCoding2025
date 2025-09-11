9d3708925b21177f5f12030a79862f4cc04f9b61
erwinnation0-creator <erwinnation0@gmail.com>
2025-09-11 17:54:56 +0800

    Create Days04.java

---
+++ Days04.java
@@ -0,0 +1,17 @@
+package days004;
+
+public class Days004 {
+
+    public static void main(String[] args) {
+        // kode (\t) menambah spasi
+        System.out.println("nama :\tErwin");
+        //kode (\\) menambah garis miring terbalik pada ouputan
+        System.out.println("contoh : \\");
+        // kode (\n) memindahkan teks ke garis baru
+        System.out.println("Muhammad \n Erwin");
+        //kode (\r) menghilangkan teks sebelumnya
+        System.out.println("halo \kak!");
+        
+    }
+    
+}

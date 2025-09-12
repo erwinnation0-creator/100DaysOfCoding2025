783741dad1898a08dfc1caca7e73a472a60bdc77
erwinnation0-creator <erwinnation0@gmail.com>
2025-09-12 23:32:14 +0800

    Create Days05.java

---
+++ Days05.java
@@ -0,0 +1,21 @@
+package day5;
+
+public class Day5 {
+
+    public static void main(String[] args) {
+       // byte  -> untuk bilangan bulat dengan ukuran 8 bit
+       byte a = 12;
+       // short  -> untuk bilangan bulat dengan ukuran 16 bit
+       short b = 21; 
+       // int -> untuk bilangan bulat dengan ukuran 32 bit
+       int c = 123456;
+       // long  -> untuk bilangan bulat dengan ukuran 64 bit
+       long d = 12345678910;
+       
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+    
+}

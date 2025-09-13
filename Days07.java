86693c6687e9c85e91e7331ee9af5130be701469
erwinnation0-creator <erwinnation0@gmail.com>
2025-09-14 05:36:57 +0800

    Create Days07.java

---
+++ Days07.java
@@ -0,0 +1,12 @@
+public class typeDataCharDanBooleanDays07 {
+  public static void main(String[] args ) {
+  char a = 'E';
+  boolean b = true;
+
+System.out.println("Inisial nama saya adalah:" + a);
+  System.out.println("Apakah sudah masuk waktu shubuh?:" + b);
+
+  }
+
+}
+  

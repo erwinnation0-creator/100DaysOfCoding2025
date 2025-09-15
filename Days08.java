6465d3dbdc81d4e4fb16c4127aa700badf92855d
erwinnation0-creator <erwinnation0@gmail.com>
2025-09-15 22:41:40 +0800

    Create Days08.java

---
+++ Days08.java
@@ -0,0 +1,16 @@
+public clas PengenalanTypeDataString.java {
+public static void main (String[] args) {
+  String nama = "Muhammad Erwin";
+  String fakultas = "Teknik";
+  String jurusan = "Informatika";
+
+  System.out.println("Nama anda siapa?:"+nama);
+  System.out.println("Kamu dari jurusan mana?:"+fakultas);
+  System.out.println("Kamu dari jurusan mana?:"+jurusan);
+
+      }
+
+
+
+
+}

2d7b38fb1c07ab4073425e0a3da5c76ae6f06e7a
erwinnation0-creator <erwinnation0@gmail.com>
2025-09-16 23:04:19 +0800

    Create Days09.java

---
+++ Days09.java
@@ -0,0 +1,38 @@
+public class JavaDays009.Java {
+
+    public static void main(String[] args) {
+       
+        
+        Byte umur ;
+        double tinggi;
+        short berat_badan;
+        
+        
+        
+        
+       umur = 19;
+       tinggi = 167.5;
+      berat_badan = 60;
+      
+      System.out.println("Usia saya adalah:"+umur+" Tahun");
+      System.out.println("Tinggi saya adalah:"+tinggi+" cm");
+      System.out.println("Berat badan saya adalah:"+berat_badan+" kg");
+      
+      System.out.println("Baiklah contoh dibawah adalah updatean variable ");
+      
+       umur = 24;
+       tinggi = 174.5;
+      berat_badan= 65;
+      
+      System.out.println("\ntahun 2030");
+      System.out.println("Usia saya sekarang adalah;"+umur+" Tahun");
+      System.out.println("Tinggi saya sekarang adalah:"+tinggi+" cm");
+      System.out.println("Berat badan saya sekarang adalah:"+berat_badan+" kg");
+      
+               
+        
+        
+        
+       }
+    
+}

be33976a18788301c01a6be3235c13d6afee2c86
erwinnation0-creator <erwinnation0@gmail.com>
2025-09-17 23:28:26 +0800

    Create Days010.java

---
+++ Days010.java
@@ -0,0 +1,17 @@
+public class Days10 {
+    public static void main(String[] args) {
+    /*Constanta (Final Variabel)
+      Constanta adalah variabel yang nilainya tetap dan tidak bisa diubah selama 
+      program berjalan.Di Java, kita membuat constanta dengan kata kunci final.
+    */
+       final double nilaiipk =3.60;
+       final double ipk_keseluruhan = 3.50;
+        System.out.println("NILAI IPK = " + nilaiipk);
+        System.out.println("IPK KESELURUHAN = " + ipk_keseluruhan);
+        
+        /*nilaiipk = 3.60; jika komentar ini dimatikan maka akan terjadi eror
+                              karena nilaiipk sudah final.
+        */
+        }
+    
+}

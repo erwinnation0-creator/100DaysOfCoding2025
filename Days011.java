bbbbe309a02213a24134ce255318f799827dccec
erwinnation0-creator <erwinnation0@gmail.com>
2025-09-18 20:16:46 +0700

    Create Days011.java

---
+++ Days011.java
@@ -0,0 +1,32 @@
+public class Days011.Java {
+
+    
+    public static void main(String[] args) {
+       
+        Scanner input = new Scanner(System.in);
+        System.out.print ("Masukan nama  : ");
+        String Nama = input.nextLine();
+        System.out.print ("Masukan prodi : ");
+        String Jurusan = input.nextLine();
+       
+        
+        
+        System.out.println("==========");
+        System.out.println("Halo Guyss Nama Saya Adalah " + Nama + " Jurusan Saya Adalah " + Jurusan);
+        
+        
+        
+       
+        
+        
+        
+        
+        
+         
+          } 
+       
+       
+        
+        
+    }
+    

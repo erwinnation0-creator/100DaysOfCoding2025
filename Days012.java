d391fbc87a41903c216e15a11f3738a113f21dd7
erwinnation0-creator <erwinnation0@gmail.com>
2025-09-19 22:24:37 +0700

    Create Days012.java

---
+++ Days012.java
@@ -0,0 +1,46 @@
+public class Days)012.java { 
+    public static void main(String[] args) {
+        
+       String Nama,Nim,Prodi,Universitas,Alamat,Alumni,Hobi,Status;
+        Float Tinggi,Berat_Badan;
+        int usia,Tanggal,Tahun;
+        String Kata_Kata;
+        Nama ="Muhammad Erwin";
+        Nim = "D0225521";
+        Prodi = "Informatika";
+        Universitas = "Sulawesi Barat";
+        Alamat = "Pamboang";
+        Alumni = "Ponpes DDI Baruga";
+        Hobi = "Bergerak";
+        Status = "Mahasiswa";
+        Tinggi = 12.5f;
+        Berat_Badan = 60.5f;
+        Kata_Kata = "Tetaplah Hidup :)";
+           
+          System.out.println("BIODATA SAYA !!!!!");
+           System.out.println("Perkenalan Nama Saya :" + Nama);
+             System.out.println("Dengan Nim :" + Nim);
+               System.out.println("Jurusan Saya Adalah :" + Prodi);
+                System.out.println("Saya Dari Universitas : "  + Universitas);
+                 System.out.println("Saya Berasal Dari :" + Alamat);
+                  System.out.println("Saya Alumni Dari Sekolah :" + Alumni);
+                   System.out.println("Hobi Saya Adalah :" + Hobi);
+                    System.out.println("Status Saya Adalah :" + Status); 
+                     System.out.println ("Adapun Tinggi Badan Saya Adalah :" + Tinggi);
+                      System.out.println("Adapun Berat Badan Saya Adalah :" + Berat_Badan);
+                       System.out.println("2 Kata Malam Ini : " + Kata_Kata);
+               
+            
+            
+       
+        
+               
+                      
+                      
+                      
+                      
+                      
+                      
+    }
+    
+}

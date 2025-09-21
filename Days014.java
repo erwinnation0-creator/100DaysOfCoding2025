05fe2054f95f3b946d457e6198e17f42d738cb07
erwinnation0-creator <erwinnation0@gmail.com>
2025-09-21 23:29:41 +0800

    Create Days014.java

---
+++ Days014.java
@@ -0,0 +1,8 @@
+public class Days014.java {
+    public static void main(String[] args) {
+        String angkaString = "3.14";            // String sumber
+        double angka = Double.parseDouble(angkaString); // String -> double
+
+        System.out.println("Hasil konversi: " + angka);
+    }
+}

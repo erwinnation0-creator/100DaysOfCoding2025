24049434e1340af8ffbb2cc25925cfd825fd11fa
erwinnation0-creator <erwinnation0@gmail.com>
2025-09-22 23:32:22 +0800

    Create Days015.java

---
+++ Days015.java
@@ -0,0 +1,14 @@
+public class Days015 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        // Penjumlahan
+        int hasilTambah = a + b;
+        System.out.println("Hasil penjumlahan: " + hasilTambah);
+
+        // Pengurangan
+        int hasilKurang = a - b;
+        System.out.println("Hasil pengurangan: " + hasilKurang);
+    }
+}

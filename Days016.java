f13879596607d9142d6d3b68e8c2ba0d553f1aa5
erwinnation0-creator <erwinnation0@gmail.com>
2025-09-23 23:50:57 +0800

    Create Days016.java

---
+++ Days016.java
@@ -0,0 +1,13 @@
+package Wynn_Days016;
+public class OperatorAritmatika {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 5;
+
+        int kali = a * b; 
+        System.out.println("Hasil perkalian = " + kali);
+
+        int bagi = a / b; 
+        System.out.println("Hasil pembagian = " + bagi);
+    }
+}

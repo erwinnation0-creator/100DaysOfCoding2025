e99c321846a5ba29c2688962553b59837a3b3753
erwinnation0-creator <erwinnation0@gmail.com>
2025-09-24 23:24:27 +0800

    Create Days017.java

---
+++ Days017.java
@@ -0,0 +1,15 @@
+package Days017;
+
+public class Wynn07 {
+    public static void main (String[] args) {
+        int a = 123;
+        int b = 12;
+        int c = a%b;
+         System.out.println("Hasil Modulusnya Adalah : " + (c));
+        
+        
+        
+        
+        
+    } 
+}

7387a8d61087ea98f702adf56b6c8ddafa45b0d3
erwinnation0-creator <erwinnation0@gmail.com>
2025-09-25 17:37:54 +0700

    Create Days018.java

---
+++ Days018.java
@@ -0,0 +1,29 @@
+package Days018;
+
+
+public class Wynn_Project {
+
+    
+    public static void main(String[] args) {
+        int Wynn = 102107;
+        long Bill = Wynn;
+         System.out.println("Hasil Konversi : " + Bill);
+         
+        float ABC = 1234.5f;
+        double DEF = ABC;
+         System.out.println("Hasil Konversi : " + DEF);
+         
+        byte UVW = 21;
+        short XYZ = UVW;
+         System.out.println("Hasil Konversi : " + XYZ);
+        
+        
+        
+                  
+         
+        
+        
+        
+    }
+    
+}

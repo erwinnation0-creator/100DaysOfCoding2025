f12e1254c5379a1cc65d4c6f32c2fc5f7e5b81d2
erwinnation0-creator <erwinnation0@gmail.com>
2025-09-27 22:54:19 +0800

    Create Days20.java

---
+++ Days20.java
@@ -0,0 +1,16 @@
+package Days20;
+public class Days20 {
+    public static void main(String[] args) {
+        int angka = 123;               
+        double desimal = 45.67;           
+        boolean status = true;            
+        String angkaStr = String.valueOf(angka);
+        String desimalStr = String.valueOf(desimal);
+        String statusStr = String.valueOf(status);
+
+        
+        System.out.println("Angka jadi String: " + angkaStr);
+        System.out.println("Desimal jadi String: " + desimalStr);
+        System.out.println("Status jadi String: " + statusStr);
+    }
+}

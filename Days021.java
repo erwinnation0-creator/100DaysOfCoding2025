d31e4a7e4e38c3d774fb86dde8676ca9d2257538
erwinnation0-creator <erwinnation0@gmail.com>
2025-09-28 23:16:07 +0800

    Create Days021.java

---
+++ Days021.java
@@ -0,0 +1,16 @@
+package Days21;
+public class TukarNilai {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Sebelum ditukar: a = " + a + ", b = " + b);
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("Sesudah ditukar: a = " + a + ", b = " + b);
+    }
+}
+

1e5311bd7aeb1b1657c4d3f345282ef3e5be812b
erwinnation0-creator <erwinnation0@gmail.com>
2025-09-29 23:47:24 +0800

    Create Days022.java

---
+++ Days022.java
@@ -0,0 +1,9 @@
+package Days022;
+public class LuasPersegiSimple {
+    public static void main(String[] args) {
+        int sisi = 5;
+        int luas = sisi * sisi;
+        System.out.println("Luas Persegi adalah: " + luas);
+    }
+}
+

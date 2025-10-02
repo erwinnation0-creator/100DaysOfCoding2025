ba69a315d7ccc4dd3348a8d44bbbd28acda370f0
erwinnation0-creator <erwinnation0@gmail.com>
2025-10-02 23:42:53 +0800

    Create Days25.java

---
+++ Days25.java
@@ -0,0 +1,39 @@
+public class IncrementDecrement {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        System.out.println("Nilai awal a: " + a);
+        a++;
+        System.out.println("Setelah a++ : " + a);
+        ++a;
+        System.out.println("Setelah ++a: " + a);
+        a--;
+        System.out.println("Setelah a-- : " + a);
+        --a;
+        System.out.println("Setelah --a: " + a);
+        System.out.println();
+
+        System.out.println("Nilai awal b: " + b);
+        b++;
+        System.out.println("Setelah b++ : " + b);
+        ++b;
+        System.out.println("Setelah ++b: " + b);
+        b--;
+        System.out.println("Setelah b-- : " + b);
+        --b;
+        System.out.println("Setelah --b: " + b);
+        System.out.println();
+
+        System.out.println("Nilai awal c: " + c);
+        c++;
+        System.out.println("Setelah c++ : " + c);
+        ++c;
+        System.out.println("Setelah ++c: " + c);
+        c--;
+        System.out.println("Setelah c-- : " + c);
+        --c;
+        System.out.println("Setelah --c: " + c);
+    }
+}

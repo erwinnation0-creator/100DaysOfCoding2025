b5ec44c1cae014801f0c342fdca4b3216b188393
erwinnation0-creator <erwinnation0@gmail.com>
2025-10-06 22:03:47 +0800

    Create Days029.java

---
+++ Days029.java
@@ -0,0 +1,18 @@
+package days029;
+
+
+
+public class Days029 {
+
+   
+    public static void main(String[] args) {
+        int a,b;
+         a = 10;
+         b = 21;
+         System.out.println("Apakah Ini Bernilai Benar ? " + (a>b));
+         System.out.println("Apakah Ini Bernilai Benar ? " + (a<b));
+         
+        
+    }
+    
+}

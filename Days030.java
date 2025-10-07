76229536023a95d9c15f5a4ad104f5bfb8d92eb1
erwinnation0-creator <erwinnation0@gmail.com>
2025-10-07 14:02:51 +0800

    Create Days030.java

---
+++ Days030.java
@@ -0,0 +1,9 @@
+package Days030;
+public class Perbandingan {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        System.out.println("a <= b: " + (a <= b));
+        System.out.println("a >= b: " + (a >= b));
+    }
+}

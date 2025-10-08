7ca5688e695ad79794ae198d02bce1bb754e613e
erwinnation0-creator <erwinnation0@gmail.com>
2025-10-08 18:56:47 +0800

    Create Days031.java

---
+++ Days031.java
@@ -0,0 +1,8 @@
+package Days031;
+public class Main {
+    public static void main(String[] args) {
+        int umur = 20;
+        boolean adalahMahasiswa = true;
+        System.out.println(umur >= 18 && adalahMahasiswa);
+    }
+}

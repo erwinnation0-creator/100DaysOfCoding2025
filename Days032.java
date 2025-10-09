49387e88e6cfb82404dfa9c16f8c71e615b6d23e
erwinnation0-creator <erwinnation0@gmail.com>
2025-10-09 19:39:29 +0800

    Create Days032.java

---
+++ Days032.java
@@ -0,0 +1,13 @@
+package days032;
+public class Days032 {
+    public static void main(String[] args) {
+        int nilai1 = 50;
+        int nilai2 = 80;
+
+        if (nilai1 > 60 || nilai2 > 60) {
+            System.out.println("Lulus");
+        } else {
+            System.out.println("Tidak Lulus");
+        }
+    }
+}

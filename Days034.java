1f579fcfde36e9cfbe8416ecf34028e9259f14df
erwinnation0-creator <erwinnation0@gmail.com>
2025-10-11 23:14:20 +0800

    Create Days034.java

---
+++ Days034.java
@@ -0,0 +1,8 @@
+package days34;
+public class Main kombinasi_operator {
+    public static void main(String[] args) {
+        int a = 10, b = 4, c = 2;
+        boolean hasil = ((a - b) * c / 2) > 5 && a != b;
+        System.out.println(hasil);
+    }
+}

67476bb810958d47df65084d9c685458508c26f5
erwinnation0-creator <erwinnation0@gmail.com>
2025-10-13 23:52:31 +0800

    Create Days036.java

---
+++ Days036.java
@@ -0,0 +1,14 @@
+public class Days036 {
+    public static void main(String[] args) {
+        int nilai = 85;
+        if (nilai >= 70) {
+            if (nilai >= 90) {
+                System.out.println("Nilai A");
+            } else {
+                System.out.println("Nilai B");
+            }
+        } else {
+            System.out.println("Nilai C");
+        }
+    }
+}

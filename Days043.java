e511799a55d2cfbf6ef1ca334ed27943daffb6c0
erwinnation0-creator <erwinnation0@gmail.com>
2025-10-20 23:27:03 +0800

    Create Days043.java

---
+++ Days043.java
@@ -0,0 +1,21 @@
+package Days043;
+import java.util.Scanner;
+
+public class Days043 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int angka;
+        System.out.print("Masukkan angka: ");
+        angka = input.nextInt();
+
+        if (angka % 3 == 0 && angka % 5 == 0) {
+            System.out.println(angka + " adalah kelipatan 3 dan 5");
+        } else if (angka % 3 == 0) {
+            System.out.println(angka + " adalah kelipatan 3");
+        } else if (angka % 5 == 0) {
+            System.out.println(angka + " adalah kelipatan 5");
+        } else {
+            System.out.println(angka + " bukan kelipatan 3 atau 5");
+        }
+    }
+}

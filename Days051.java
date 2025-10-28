a242a5bc31379db8630d89575653de2d6315e2dd
erwinnation0-creator <erwinnation0@gmail.com>
2025-10-28 23:26:51 +0800

    Create Days051.java

---
+++ Days051.java
@@ -0,0 +1,8 @@
+public class PerulanganFor {
+    public static void main(String[] args) {
+        // Perulangan for dari 1 sampai 5
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Perulangan ke-" + i);
+        }
+    }
+}

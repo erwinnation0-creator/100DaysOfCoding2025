b1e7fe463f1991c6af0c058b2b107b64ea8fa236
erwinnation0-creator <erwinnation0@gmail.com>
2025-10-29 23:51:57 +0800

    Add Days052.java with a simple loop example

---
+++ Days052.java
@@ -0,0 +1,12 @@
+package Days052;
+
+public class Days052 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 5) {
+            System.out.println("Perulangan ke-" + i);
+            i++;
+        }
+        System.out.println("Perulangan selesai!");
+    }
+}

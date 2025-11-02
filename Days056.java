47def26788f1f07eda6e602f7a7d9b4ae9923c06
erwinnation0-creator <erwinnation0@gmail.com>
2025-11-02 23:06:23 +0800

    Add Days056.java with a do-while loop example

---
+++ Days056.java
@@ -0,0 +1,13 @@
+package Days056;
+public class Days056 {
+    public static void main(String[] args) {
+        int angka = 1;
+
+        do {
+            System.out.println("Angka ke-" + angka);
+            angka++;
+        } while (angka <= 5);
+
+        System.out.println("Perulangan selesai.");
+    }
+}

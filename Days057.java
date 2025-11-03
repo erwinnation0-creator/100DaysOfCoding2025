7de91b4a11e85be0dff72891a290c9b673707077
erwinnation0-creator <erwinnation0@gmail.com>
2025-11-03 21:03:45 +0800

    Add Days057 class with nested loops for output

---
+++ Days057.java
@@ -0,0 +1,15 @@
+package days057;
+
+public class Days057 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <=3; i++) {
+            for (int j = 1; j <= 4; j++) {
+                System.out.println("* ");
+                
+            }
+            System.out.println();   
+        }
+    }
+    
+}

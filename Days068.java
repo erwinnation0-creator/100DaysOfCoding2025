4e4a92082a3738c2a9df3ad7511ab55d63fde467
erwinnation0-creator <erwinnation0@gmail.com>
2025-11-14 22:29:42 +0800

    Add Java program to print a square of asterisks

---
+++ Days068.java
@@ -0,0 +1,14 @@
+package Days068;
+
+public class Days068 {
+    public static void main(String[] args) {
+        int n = 5; // ukuran persegi
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}

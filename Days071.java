4bafce11a65d8cd51e75ec89e47170a01cf3875e
erwinnation0-creator <erwinnation0@gmail.com>
2025-11-17 23:02:07 +0800

    Add triangle pattern printing in Days071.java

---
+++ Days071.java
@@ -0,0 +1,14 @@
+package Days071;
+
+public class Days071 {
+    public static void main(String[] args) {
+        int tinggi = 5;
+
+        for (int i = 1; i <= tinggi; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

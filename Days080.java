deecdba3c38d0182440c26bcc2379536aee2b930
erwinnation0-creator <erwinnation0@gmail.com>
2025-11-26 22:58:35 +0800

    Create Days080.java

---
+++ Days080.java
@@ -0,0 +1,19 @@
+
+package days080;
+import java.util.Scanner;
+
+public class Days080 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+         String [] Binatang = {"ayam","sapi","kambing","kerbau","domba"};
+          int Jumlah = Binatang.length;
+        for (int i = 0; i < Binatang.length; i++) {
+            System.out.println("Hewan ke-"+ i +" " + Binatang[i]+ "  ");  
+        }
+        System.out.println(" ");  
+          
+      
+    }
+    
+}

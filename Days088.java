90dd3ea41a460e9fe47027f71c18411d200b7aa1
erwinnation0-creator <erwinnation0@gmail.com>
2025-12-04 22:40:35 +0800

    Add Days088.java to list fruits

---
+++ Days088.java
@@ -0,0 +1,13 @@
+package coding100days;
+
+public class Days088 {
+    public static void main(String[] args) {
+        
+        String[] buah = {"Apel", "Mangga", "Jeruk", "Anggur"};
+
+        System.out.println("Daftar buah:");
+        for (String b : buah) {
+            System.out.println(b);
+        }
+    }
+}

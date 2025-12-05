9f9ce1cd46cae297153a193cd6f7cee513fe0fdd
erwinnation0-creator <erwinnation0@gmail.com>
2025-12-05 23:29:52 +0800

    Create Days089.java

---
+++ Days089.java
@@ -0,0 +1,9 @@
+package coding100days;
+public class Days089 {
+    public static void teks() {
+        System.out.println("Pemula");
+    }
+    public static void main(String[] args) {
+        teks();
+    }
+}

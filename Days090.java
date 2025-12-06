c864374ea58f3d605c9c460ecb780ce44ad64320
erwinnation0-creator <erwinnation0@gmail.com>
2025-12-06 23:52:18 +0800

    Add Wynn class with main and coba method

---
+++ Days090.java
@@ -0,0 +1,15 @@
+package Days090;
+
+public class Wynn {
+
+    public static void main(String[] args) {
+
+        System.out.println(coba(10));
+    }
+
+    public static int coba(int a) {
+        
+       return a; 
+    }
+
+}
